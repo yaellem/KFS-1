8cd577a60946f2a96689a258de0680a4a7dfaa31
Yaelle MARCILLY <ymarcill@made-f0Br3s2.clusters.42paris.fr>
2024-05-24 17:41:54 +0200

    Add macros & Add scrolling management

--- kernel.c
+++ kernel.c
@@ -41,6 +41,10 @@ enum vga_color {
 #define VGA_OFFSET_LOW 		0x0f
 #define VGA_OFFSET_HIGH 	0x0e
 
+#define VIDEO_ADDRESS 		0xb8000
+#define VGA_WIDTH 			80
+#define  VGA_HEIGHT			25
+
 
 // Copies value from DX :-> port ==> AL :-> result . READS THE PORT
 unsigned char port_byte_in(unsigned short port) {
@@ -86,17 +90,16 @@ static inline uint16_t vga_entry(unsigned char uc, uint8_t color)
 	return (uint16_t) uc | (uint16_t) color << 8;
 }
  
-size_t strlen(const char* str) 
+
+size_t strlenk(const char* str) 
 {
 	size_t len = 0;
 	while (str[len])
 		len++;
 	return len;
 }
- 
-static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 25;
- 
+
+
 size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_color;
@@ -107,7 +110,7 @@ void terminal_initialize(void)
 	terminal_row = 0;
 	terminal_column = 0;
 	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-	terminal_buffer = (uint16_t*) 0xB8000;
+	terminal_buffer = (uint16_t*)VIDEO_ADDRESS;
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
 			const size_t index = y * VGA_WIDTH + x;
@@ -126,21 +129,34 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 	const size_t index = y * VGA_WIDTH + x;
 	terminal_buffer[index] = vga_entry(c, color);
 }
+
+void scroll_term(int line) {
+	int index_w = 0;
+	int end_pos = VGA_WIDTH * VGA_HEIGHT;
+
+	for (int index_r = VGA_WIDTH * line; index_r < end_pos + VGA_WIDTH; index_r++) {
+		terminal_buffer[index_w++] = terminal_buffer[index_r];
+	}
+	terminal_row--;
+	// terminal_row = 0;
+}
+
 // void terminal_putchar(char c) 
 int terminal_putchar(char c) 
 {
 	/* Newline management */
 	if (c == '\n') {
 		terminal_column = 0;
+		/* Scrolling management */
 		if (++terminal_row == VGA_HEIGHT)
-			terminal_row = 0;
+			scroll_term(1);
 	} else {
 		terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
 		if (++terminal_column == VGA_WIDTH) {
 			terminal_column = 0;
 		/* Scrolling management */
 			if (++terminal_row == VGA_HEIGHT)
-				terminal_row = 0;
+				scroll_term(1);
 		}
 	}
 	
@@ -155,6 +171,7 @@ void terminal_write(const char* data, size_t size, int cursor_offset)
 	for (i = 0; i < size; i++) {
 		cursor_offset = terminal_putchar(data[i]);
 	}
+	/* Cursor management */
 	set_cursor(cursor_offset * 2);
 }
 
